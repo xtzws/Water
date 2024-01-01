da4f48625e646dae3c65930ad7cf395a28c721d6
Isaac Greene <irgreene@icloud.com>
2024-01-01 16:13:10 -0500

    Initial Commit

---
+++ Water/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water
+//
+//  Created by Isaac Greene on 2024-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by Isaac Greene on 2024-01-01.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
